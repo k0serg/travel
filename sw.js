f9d268f571eeefa05fdbafffe7f4d67170dceaba
k0serg <k0serg@internet.ru>
2026-07-11 11:54:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'vacation-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Установка и кэширование файлов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Отдача файлов из кэша, если нет интернета
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Возвращаем из кэша, если есть, иначе идем в сеть
+        return response || fetch(event.request);
+      })
+  );
+});
